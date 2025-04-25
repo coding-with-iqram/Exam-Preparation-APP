caed32b1ee06b5c306047c946ec16a8fa233bd06
coding-with-iqram <codingwithiqram@gmail.com>
2025-04-25 15:58:50 +0600

    Initial commit

---
+++ Home.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+export default Home;
